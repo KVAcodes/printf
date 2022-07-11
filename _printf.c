a06fa76eb949b183616c4d7a5bcbfc424c892901
Kadiri victor <kadiriavictor@gmail.com>
2022-07-11 23:48:59 +0000

    added main.h

--- _printf.c
+++ _printf.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+/**
+ * _printf - produces output according to a format
+ * @format: format is a character string
+ * Return: The number of characters printed (excluding the null 
+ * byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
 
+}
